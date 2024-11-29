5d8a0c7ce1f2d2654f5062e22424c867aa0b8856
hungndweq <thang7781@gmail.com>
2024-11-29 22:22:52 +0700

    Update files navbar and hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
   }
 
   return (
-    <section className='w-full nav-height bg-black relative'>
+    <section className='w-full nav-height bg-black items-center mr-10 relative'>
       <div className='h-5/6 w-full flex-center flex-col'>
         <p id='hero' className='hero-title'>iPhone 15 Pro</p>
         <div className='md:w-10/12 w-9/12'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { appleImg, searchImg, bagImg } from "../utils";
 import { navLists } from "../constants";
 const Navbar = () => {
   return (
-    <header className="w-full py-5:px-10 px-5 flex justify-between items-center mt-5">
+    <header className="w-full px-5 flex justify-between items-center mt-5">
       <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" width={14} height={18} />
-        <div className="flex flex-1 justify-center max-sm:hidden">
+        <div className="flex flex-1 justify-center items-center ml-12 max-sm:hidden">
           {navLists.map((nav) => (
             <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all duration-300">
               {nav}
