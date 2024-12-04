1446cda5e5937a203bfea7bd94655a6bfaefa781
hungndweq <thang7781@gmail.com>
2024-12-04 22:44:25 +0700

    Update 3D model

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-
+import reactThree from '@react-three'
 export default [
   { ignores: ['dist'] },
   {
@@ -22,6 +22,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@react-three': reactThree,
     },
     rules: {
       ...js.configs.recommended.rules,
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-
-const ModelView = () => {
+import { Html, OrbitControls, View,PerspectiveCamera } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import Lights from './Light'
+import Iphone from './Iphone'
+import * as THREE from 'three'
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
     return (
-        <div>
-            ModelView
-        </div>
+        <View
+            index={index}
+            id={gsapType}
+            className={`w-full h-full $ {index === 2} ? "right-[-100%]:''`}
+        >
+            <ambientLight intensity={0.3} />
+
+            <perspectiveCamera makeDefault position={[0, 0, 4]} />
+            <Lights />
+            <OrbitControls
+                makeDefault
+                ref={controlRef}
+                enableZoom={false}
+                enablePan={false}
+                rotateSpeed={0.4}
+                target={new THREE.Vector3(0, 0, 0)}
+                onEnded={() => setRotationSate(controlRef.current.getAzimuthaAngle())}
+
+
+            />
+
+            <group ref={groupRef} name={`${index === 1} ? 'small':'large'`} position={[0, 0, 0]}>
+                <Suspense fallBack={<Html><div>Loading</div></Html>}>
+                    <Iphone
+                        scale={index === 1 ? [25, 25, 25] : [10,10,10]}
+                        item={item}
+                        size={size}
+                    />
+                </Suspense>
+            </group>
+
+        </View >
     )
 }
 
