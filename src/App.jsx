21efb10aba4df38a01c21c062e383c00db50a8f0
hungndweq <thang7781@gmail.com>
2024-12-10 13:52:58 +0700

    Update projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Highlights from "./components/Highlights";
-import Model from "./components/Model";
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+
+import * as Sentry from '@sentry/react';
+
 const App = () => {
+  return <button onClick={() => {throw new Error("This is your first error!");}}>Break the world</button>;
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
       <Model />
+     
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://ccb5f1eb809885f2b02952817da888f4@o4508441649807365.ingest.us.sentry.io/4508441896091648",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
